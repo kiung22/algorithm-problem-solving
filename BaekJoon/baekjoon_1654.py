a2e6b71b9edee9dc985716bbd6e6c99cf85fd7b6
kiung <rldnd22@gmail.com>
2021-02-19 22:46:29 +0900

    2021.02.19

--- BaekJoon/baekjoon_1654.py
+++ BaekJoon/baekjoon_1654.py
@@ -1,18 +1,21 @@
-K, N = map(int, input().split())
-wires = [int(input()) for _ in range(K)]
+import sys
 
-lans =[]
-for wire in wires:
-    for i in range(1, N - K + 2):
-        lans.append(wire // i)
-lans.sort()
+K, N = map(int, sys.stdin.readline().split())
+wires = list(map(int, [sys.stdin.readline() for _ in range(K)]))
+wires.sort(reverse=True)
 
-for lan in lans:
-    n = 0
-    for wire in wires:
-        n += wire // lan
-    if n < N:
-        break
-    result = lan
+LAN = []    # 가능한 랜선의 길이를 담은 리스트
+for i in range(K):
+    k = (i + 1) * K
+    for j in range(1, N - k + 2):
+        LAN.append(wires[i] // j)
+LAN.sort() # 이진탐색을 위해 정렬
+print(LAN)
+
+# 이진탐색으로 N개 이상의 랜선이 나오는 길이 중 가장 긴 길이를 찾는다.
+left = 0
+right = len(LAN) - 1
+while left <= right:
+    middle = (left + right) // 2
 
 print(result)
--- BaekJoon/baekjoon_1874.py
+++ BaekJoon/baekjoon_1874.py
@@ -1,11 +1,12 @@
-N = int(input())
+import sys
+
+N = int(sys.stdin.readline())
+input_data = map(int, [sys.stdin.readline() for _ in range(N)])
 
 number = 1
 stack = []
-ans = []    # 출력할 '+', '-'을 담을 변수
-for _ in range(N):
-    n = int(input())
-
+ans = []    # 출력할 '+', '-'을 담을 list
+for n in input_data:
     # numbers의 마지막 원소가 입력받은 n 보다 커질때까지 stack에 push한다.
     while number <= n:
         stack.append(number)
@@ -13,10 +14,11 @@ for _ in range(N):
         ans.append('+')
 
     # stack pop의 결과가 n과 같으면 성공, 다르면 불가능이므로 'No'를 출력하고 종료
-    if stack.pop() == n:
+    if stack[-1] == n:
+        stack.pop()
         ans.append('-')
     else:
-        ans = ['No']
+        ans = ['NO']
         break
 
 for a in ans:
