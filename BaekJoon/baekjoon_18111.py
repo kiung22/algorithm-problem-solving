1505c63a225b4537e4187e590c0934a4c928c7ad
kiung <rldnd22@gmail.com>
2021-02-25 17:03:44 +0900

    2021.02.25

--- BaekJoon/baekjoon_18111.py
+++ BaekJoon/baekjoon_18111.py
@@ -6,3 +6,29 @@ for row in blocks:
     for block in row:
         count[block] += 1
 
+for i in range(257):
+    if count[i]:
+        L = i
+        break
+
+for i in range(256, -1, -1):
+    if count[i]:
+        R = i
+        break
+
+time = 0
+while L < R:
+    if count[L] <= B and count[L] <= count[R] * 2:
+        B -= count[L]
+        time += count[L]
+        count[L+1] += count[L]
+        count[L] = 0
+        L += 1
+    else:
+        B += count[R]
+        time += count[R] * 2
+        count[R-1] += count[R]
+        count[R] = 0
+        R -= 1
+
+print(time, L)
