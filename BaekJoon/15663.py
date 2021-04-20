e10f40d1b30bdeb691eeb6adc4c7ab489ca1c1b3
kiung <rldnd22@gmail.com>
2021-04-20 17:51:08 +0900

    2021.04.20

--- BaekJoon/15663.py
+++ BaekJoon/15663.py
@@ -6,24 +6,23 @@ def f(n, k, s):
     if k == M:
         perm.add(s)
         return
-    if n == N:
+    if n == N or k + N-n+1 < M:
         return
 
     for i in range(N):
-        if used[int(numbers[i])] == 0:
-            used[int(numbers[i])] = 1
-            f(n+1, k+1, s + numbers[n])
-            used[int(numbers[i])] = 0
+        if used[i] == 0:
+            used[i] = 1
+            f(n+1, k+1, s + ' ' + numbers[i])
+            used[i] = 0
 
 
 N, M = map(int, input().split())
 numbers = input().split()
 
-numbers.sort()
-used = [0] * 10001
+used = [0] * N
 perm = set()
 f(0, 0, '')
 perm = sorted(list(perm))
 
 for p in perm:
-    print(*p)
+    print(p.lstrip())
