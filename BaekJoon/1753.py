18ee1675e1ed2f46c241ef61433ef4e1bcd25836
kiung <rldnd22@gmail.com>
2021-04-23 17:59:46 +0900

    2021.04.23

--- BaekJoon/1753.py
+++ BaekJoon/1753.py
@@ -5,26 +5,22 @@ input = sys.stdin.readline
 
 def dijkstra(s):
     used = [0] * (V+1)
-    used[s] = 1
-    dist = ['INF'] * (V+1)
-    dist[s] = 0
-    for v, w in adj[s]:
-        dist[v] = w
+    dist = [INF] * (V+1)
+    heap = [(0, s)]
 
-    for _ in range(V-1):
-        heap = []
-        min_value = INF
-        for v in range(1, V+1):
-            if used[v] == 0 and dist[v] != 'INF' and dist[v] < min_value:
-                w = v 
-                min_value = dist[v]
-        used[w] = 1
-        for v in range(1, V+1):
-            dist[v] = min(dist[v], dist[w] + adj[w][v])
+    while heap:
+        w, u = heapq.heappop(heap)
+        if used[u] == 0:
+            used[u] = 1
+            dist[u] = w
+            for v, d in adj[u]:
+                if used[v] == 0 and dist[u] + d < dist[v]:
+                    heapq.heappush(heap, (dist[u] + d, v))
     
     return dist
 
 
+INF = 100000000
 V, E = map(int, input().split())
 S = int(input())
 
@@ -35,4 +31,7 @@ for _ in range(E):
 
 dist = dijkstra(S)
 for i in range(1, V+1):
-    print(dist[i])
+    if dist[i] != INF:
+        print(dist[i])
+    else:
+        print('INF')
