db95f3451d1e7f349f03af7f0e785297a3e551a8
kiung <kiung22@naver.com>
2021-04-26 22:01:54 +0900

    2021.04.26

--- BaekJoon/12851.py
+++ BaekJoon/12851.py
@@ -4,34 +4,31 @@ from collections import deque
 input = sys.stdin.readline
 
 def bfs(n, k):
-    if n == k:
-        print(0)
-        print(1)
-        return
     q = deque([n])
     visited = [0] * 100001
     visited[n] = 1
-    cnt = 0
+    visited_time = [0] * 100001
+    visited_time[n] = 0
+    time = 0
 
     while q:
         qq = deque()
-        visited_now = visited[:]
-        cnt += 1
+        time += 1
         while q:
             n = q.popleft()
             for x in [n-1, n+1, n*2]:
-                if 0 <= x <= 100000 and visited[x] == 0:
-                    if visited_now[x] == 0:
+                if 0 <= x <= 100000:
+                    if visited[x] == 0:
                         qq.append(x)
-                        visited_now[x] = visited[n]
-                    else:
-                        visited_now[x] += 1
-        if visited_now[k]:
-            print(cnt)
-            print(visited_now[k])
+                        visited[x] += visited[n]
+                        visited_time[x] = time
+                    elif visited_time[x] == time:
+                        visited[x] += visited[n]
+
+        if visited[k]:
+            print(visited_time[k], visited[k], sep='\n')
             return
         q = qq
-        visited = visited_now
 
 
 N, K = map(int, input().split())
