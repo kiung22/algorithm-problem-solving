0af706ef9625c2346ad62948089fdf50022ea749
kiung <rldnd22@gmail.com>
2021-04-26 19:42:53 +0900

    2021.04.26

--- BaekJoon/9663.py
+++ BaekJoon/9663.py
@@ -2,23 +2,22 @@ import sys
 
 input = sys.stdin.readline
 
-def queen(i, j):
+def queen(i):
     global cnt
 
-    if i == N-1:
+    if i == N:
         cnt += 1
         return
 
-    i += 1
-    for k in range(N):
-        if col[k] == 0 and right[N-1-i+k] == 0 and left[i+k] == 0:
-            col[k] = 1
-            right[N-1-i+k] = 1
-            left[i+k] = 1
-            queen(i, k)
-            col[k] = 0
-            right[N-1-i+k] = 0
-            left[i+k] = 0
+    for j in range(N):
+        if col[j] == 0 and right[N-1-i+j] == 0 and left[i+j] == 0:
+            col[j] = 1
+            right[N-1-i+j] = 1
+            left[i+j] = 1
+            queen(i+1)
+            col[j] = 0
+            right[N-1-i+j] = 0
+            left[i+j] = 0
 
 
 N = int(input())
@@ -27,16 +26,9 @@ arr = [[0]*N for _ in range(N)]
 
 cnt = 0
 col = [0] * N
-right = [0] * (N*2-1)
-left = [0] * (N*2-1)
+right = [0] * (N+N-1)
+left = [0] * (N+N-1)
 
-for j in range(N):
-    col[j] = 1
-    right[N-1+j] = 1
-    left[j] = 1
-    queen(0, j)
-    col[j] = 0
-    right[N-1+j] = 0
-    left[j] = 0
+queen(0)
 
 print(cnt)
