462c8803a369ef5fe227fdbbdb515ca4a319033e
kiung <kiung22@naver.com>
2022-01-10 17:53:34 +0900

    2022.01.10

--- BaekJoon/1007.py
+++ BaekJoon/1007.py
@@ -2,55 +2,38 @@ import sys
 
 input = sys.stdin.readline
 
-def f(n):
-    global r, c, min_value, count
-    count += 1
+def f(n, cnt):
+    global r, c, min_value
 
-    if n == N // 2:
-        # 벡터 합의 길이 계산
-        result = r**2 + c**2
+    if cnt + N - n < N // 2:
+        return
+
+    if cnt == N // 2:
+        result = (total_r - 2*r)**2 + (total_c - 2*c)**2
         min_value = min(min_value, result)
         return
 
-    for i in range(N):
-        if visited[i] == 1:
-            continue
-
-        visited[i] = 1
-        for j in range(N):
-            if visited[j] == 1:
-                continue
-            r += dist_r[i][j]
-            c += dist_c[i][j]
-            visited[j] = 1
-            f(n+1)
-            r -= dist_r[i][j]
-            c -= dist_c[i][j]
-            visited[j] = 0
-        visited[i] = 0
+    for i in range(n, N//2 + cnt + 1):
+        r += nodes[i][0]
+        c += nodes[i][1]
+        f(i+1, cnt+1)
+        r -= nodes[i][0]
+        c -= nodes[i][1]
     
 
 T = int(input())
 for _ in range(T):
     N = int(input())
     nodes = [list(map(int, input().split())) for _ in range(N)]
-    dist_r = [[0]*N for _ in range(N)]
-    dist_c = [[0]*N for _ in range(N)]
-    for i in range(N):
-        ri, ci = nodes[i]
-        for j in range(i+1, N):
-            rj, cj = nodes[j]
-            dist_r[i][j] = rj - ri
-            dist_c[i][j] = cj - ci
-            dist_r[j][i] = -dist_r[i][j]
-            dist_c[j][i] = -dist_c[i][j]
 
-    visited = [0] * N
+    total_r = 0
+    total_c = 0
+    for r, c in nodes:
+        total_r += r
+        total_c += c
     r = 0
     c = 0
     min_value = 1000000000000
-    count = 0
-    f(0)
+    f(0, 0)
 
     print(min_value ** 0.5)
-    print(count)
